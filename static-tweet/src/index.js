41c68ea13e94c7391fdf5d6644b899b0cbcf5437
Andreea Năstase <andreanastase@gmail.com>
2020-02-03 13:26:30 +0100

    Add static tweet elements

--- static-tweet/src/index.js
+++ static-tweet/src/index.js
@@ -3,7 +3,59 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Tweet() {
-  return <div className="tweet">Tweet</div>;
+  return (
+    <div className="tweet">
+      <Avatar />
+      <div className="content">
+        {' '}
+        <Author />
+        <Time /> <Message />
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
+      </div>
+    </div>
+  );
 }
+
+function Avatar() {
+  return (
+    <img
+      src="https://www.gravatar.com/avatar/nothing"
+      className="avatar"
+      alt="avatar"
+    />
+  );
+}
+
+function Message() {
+  return (
+    <div className="message">This is a message with fewer than 140 characters.</div>
+  );
+}
+
+function Author() {
+  return (
+    <span className="author">
+      <span className="name">Andreea Nastase</span>
+      <span className="handle">@diemkay</span>
+    </span>
+  );
+}
+
+const Time = () => <span className="time">3h ago</span>;
+
+const ReplyButton = () => <i className="fa fa-reply reply-button" />;
+
+const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
+
+const LikeButton = () => <i className="fa fa-heart like-button" />;
+
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button" />
+);
 
 ReactDOM.render(<Tweet />, document.getElementById('root'));
