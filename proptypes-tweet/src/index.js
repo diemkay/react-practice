e4b7ff1cef3e63d0ebccd9866f4b82e13471c7ed
Andreea Năstase <andreanastase@gmail.com>
2020-02-07 14:20:02 +0100

    Add proptypes for several components

--- proptypes-tweet/src/index.js
+++ proptypes-tweet/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment';
+import PropTypes from 'prop-types';
 import './index.css';
 
 function Tweet({ tweet }) {
@@ -22,14 +23,42 @@ function Tweet({ tweet }) {
   );
 }
 
+Tweet.propTypes = {
+  tweet: PropTypes.object.isRequired,
+};
+
+// It’s also good to follow the DRY (Don’t Repeat Yourself) principle. If you have an explicit object shape required in one place, for instance in Author, there’s little value in duplicating the shape in the parent Tweet component. If the shape of author changes some day, there will be two places to update code. Having that second check doesn’t buy you anything, and instead, could actually cost you time in the future.
+
+// Tweet.propTypes = {
+//   tweet: PropTypes.shape({
+//     message: PropTypes.string,
+//     gravatar: PropTypes.string,
+//     author: PropTypes.shape({
+//       handle: PropTypes.string.isRequired,
+//       name: PropTypes.string.isRequired,
+//     }).isRequired,
+//     likes: PropTypes.number,
+//     retweets: PropTypes.number,
+//     timestamp: PropTypes.string,
+//   }).isRequired,
+// };
+
 function Avatar({ hash }) {
   const url = `https://www.gravatar.com/avatar/${hash}`;
   return <img src={url} className="avatar" alt="avatar" />;
 }
 
+Avatar.propTypes = {
+  hash: PropTypes.string,
+};
+
 function Message({ text }) {
   return <div className="message">{text}</div>;
 }
+
+Message.propTypes = {
+  message: PropTypes.string,
+};
 
 function Author({ author }) {
   const { name, handle } = author;
@@ -41,9 +70,20 @@ function Author({ author }) {
   );
 }
 
+Author.propTypes = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    handle: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
 const Time = ({ time }) => {
   const timeString = moment(time).fromNow();
   return <span className="time">{timeString}</span>;
+};
+
+Time.propTypes = {
+  time: PropTypes.string,
 };
 
 const ReplyButton = () => <i className="fa fa-reply reply-button" />;
@@ -68,6 +108,10 @@ const LikeButton = ({ count }) => (
     {count > 0 && <span className="like-count">{count} </span>}
   </span>
 );
+
+LikeButton.propTypes = {
+  count: PropTypes.number,
+};
 
 const MoreOptionsButton = () => <i className="fa fa-ellipsis-h more-options-button" />;
 
