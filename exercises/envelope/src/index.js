1b29c03399e82112001ccb932dabbb792962003c
Andreea Năstase <andreanastase@gmail.com>
2020-02-08 13:31:09 +0100

    Remove comments

--- exercises/envelope/src/index.js
+++ exercises/envelope/src/index.js
@@ -21,7 +21,6 @@ function Stamp() {
   );
 }
 
-// Create an Envelope component that takes toPerson and fromPerson as props and uses your AddressLabel from Exercise 1 to display the return address and the recipient address.
 function Envelope({ toPerson, fromPerson }) {
   return (
     <div className="envelope">
