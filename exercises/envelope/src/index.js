1c68c3d91edb209ed84bbf89a270a03307309937
Andreea Năstase <andreanastase@gmail.com>
2020-02-08 12:38:28 +0100

    Finish envelope exercise

---
+++ exercises/envelope/src/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function AddressLabel({ person }) {
+  const { street, city, state, zipCode } = person.address;
+  return (
+    <span className="address">
+      {`${person.name}
+       ${street}
+       ${city}, ${state} ${zipCode}`}
+    </span>
+  );
+}
+
+function Stamp() {
+  return (
+    <div className="stamp">
+      <span className="stamp-text">Some stamp</span>
+    </div>
+  );
+}
+
+// Create an Envelope component that takes toPerson and fromPerson as props and uses your AddressLabel from Exercise 1 to display the return address and the recipient address.
+function Envelope({ toPerson, fromPerson }) {
+  return (
+    <div className="envelope">
+      <div className="from-person">
+        <AddressLabel person={fromPerson} />
+      </div>
+      <div className="to-person">
+        <AddressLabel person={toPerson} />
+      </div>
+      <Stamp />
+    </div>
+  );
+}
+
+const toPerson = {
+  name: 'Mrs. Receiver',
+  address: {
+    street: '123 Fake St.',
+    city: 'San Francisco',
+    state: 'CA',
+    zipCode: '94101',
+  },
+};
+
+const fromPerson = {
+  name: 'Mr. Sender',
+  address: {
+    street: '123 Fake St.',
+    city: 'Boston',
+    state: 'MA',
+    zipCode: '02118',
+  },
+};
+
+ReactDOM.render(
+  <Envelope fromPerson={fromPerson} toPerson={toPerson} />,
+  document.getElementById('root')
+);
